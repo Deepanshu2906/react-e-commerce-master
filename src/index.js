cc9222852ad3b856f51ca797457fa3c1c8431429
Deepanshu2906 <deepanshugoyal2906@gmail.com>
2022-12-05 21:03:36 +0530

    CreateContext API start

--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Home from "./pages/Home/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import Register from "./pages/Register/Register";
-import Login from "./pages/Login/Login";
-import Cart from "./pages/Cart/Cart";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/register" element={<Register />}></Route>
-      <Route path="/signin" element={<Login />}></Route>
-      <Route path="/cart" element={<Cart />}></Route>
-      <Route path="/*" element={<p> Page not found, 404</p>}></Route>
-    </Routes>
-  </BrowserRouter>
+  <App />
 );
